9af3984328a4858600bb7f9567447beb1904df27
Emilio B. Pedrollo <emiliopedrollo@gmail.com>
2019-09-04 05:05:55 -0300

    Add search form as a Vue component

---
+++ resources/js/components/EducadexSearch.vue
@@ -0,0 +1,32 @@
+<template>
+    <form method="GET" :action="action">
+        <h1 class="display-1 text-center">Educadex<span>alpha</span></h1>
+        <div class="input-group mb-3">
+            <input type="text" class="form-control"
+                   name="search" :value="question"
+                   :placeholder=i18n.search_placeholder
+                   :aria-label=i18n.search_placeholder
+                   aria-describedby="button-search">
+            <div class="input-group-append">
+                <input class="btn btn-outline-secondary" type="submit" id="button-search"
+                       :value="i18n.make_question"/>
+            </div>
+        </div>
+    </form>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        },
+        data() {
+            return {
+                i18n: window.i18n
+            }
+        },
+        props: [
+            'action', 'question'
+        ]
+    }
+</script>
