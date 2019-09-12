1ce98a61af69c8f41f9d69a12f129a7d41a1cd1d
Emilio B. Pedrollo <emiliopedrollo@gmail.com>
2019-09-12 01:01:34 -0300

    Remove unused methods from SearchController

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -20,7 +20,6 @@ use Google\Cloud\Language\V1\PartOfSpeech\Proper;
 use Google\Cloud\Language\V1\PartOfSpeech\Reciprocity;
 use Google\Cloud\Language\V1\PartOfSpeech\Tense;
 use Google\Cloud\Language\V1\PartOfSpeech\Voice;
-use Google\Cloud\Language\V1\Sentence;
 use Google\Cloud\Language\V1\Token;
 use Google\Cloud\Language\V1\AnnotateTextRequest\Features;
 use Google\Cloud\Language\V1\Document\Type;
@@ -88,112 +87,6 @@ class SearchController extends Controller
             $output .= sprintf(PHP_EOL);
         }
         return $output;
-    }
-
-    /**
-     * @param $text
-     * @param null $projectId
-     * @throws ApiException
-     * @throws ValidationException
-     */
-    function analyze_entities($text, $projectId = null)
-    {
-        // Create the Natural Language client
-        $languageServiceClient = new LanguageServiceClient(['projectId' => $projectId]);
-        try {
-            $entity_types = [
-                0 => 'UNKNOWN',
-                1 => 'PERSON',
-                2 => 'LOCATION',
-                3 => 'ORGANIZATION',
-                4 => 'EVENT',
-                5 => 'WORK_OF_ART',
-                6 => 'CONSUMER_GOOD',
-                7 => 'OTHER',
-                8 => 'PHONE_NUMBER',
-                9 => 'ADDRESS',
-                10 => 'DATE',
-                11 => 'NUMBER',
-                12 => 'PRICE'
-            ];
-            $document = new Document();
-            // Add text as content and set document type to PLAIN_TEXT
-            $document->setContent($text)->setType(1);
-            // Call the analyzeEntities function
-            $response = $languageServiceClient->analyzeEntities($document, []);
-            $entities = $response->getEntities();
-            // Print out information about each entity
-            foreach ($entities as $entity) {
-                printf('Name: %s' . '<br/>', $entity->getName());
-                printf('Type: %s' . '<br/>', $entity_types[$entity->getType()]);
-                printf('Salience: %s' . '<br/>', $entity->getSalience());
-                if ($entity->getMetadata()->offsetExists('wikipedia_url')) {
-                    printf('Wikipedia URL: %s' . '<br/>', $entity->getMetadata()->offsetGet('wikipedia_url'));
-                }
-                if ($entity->getMetadata()->offsetExists('mid')) {
-                    printf('Knowledge Graph MID: %s' . '<br/>', $entity->getMetadata()->offsetGet('mid'));
-                }
-                printf('<br/>');
-            }
-        } finally {
-            $languageServiceClient->close();
-        }
-    }
-
-    /**
-     * @param $text
-     * @param null $projectId
-     * @throws ApiException
-     * @throws ValidationException
-     */
-    function analyze_syntax($text, $projectId = null)
-    {
-        // Create the Natural Language client
-        $languageServiceClient = new LanguageServiceClient(['projectId' => $projectId]);
-
-        try {
-            $tag_types = [
-                0 => 'UNKNOWN',
-                1 => 'ADJ',
-                2 => 'ADP',
-                3 => 'ADV',
-                4 => 'CONJ',
-                5 => 'DET',
-                6 => 'NOUN',
-                7 => 'NUM',
-                8 => 'PRON',
-                9 => 'PRT',
-                10 => 'PUNCT',
-                11 => 'VERB',
-                12 => 'X',
-                13 => 'AFFIX',
-            ];
-            // Create a new Document
-            $document = new Document();
-            // Add text as content and set document type to PLAIN_TEXT
-            $document->setContent($text)->setType(Document\Type::PLAIN_TEXT);
-            // Call the analyzeEntities function
-            $response = $languageServiceClient->analyzeSyntax($document, []);
-
-            $sentences = $response->getSentences();
-            foreach ($sentences as $sentence) {
-                /** @var Sentence $sentence */
-                printf('Sentence: %s <br/>',$sentence->serializeToJsonString());
-                print('<br/>');
-            }
-
-            $tokens = $response->getTokens();
-            // Print out information about each entity
-            foreach ($tokens as $token) {
-                /** @var Token $token */
-                printf('Token text: %s' . '<br/>', $token->getText()->getContent());
-                printf('Token part of speech: %s' . '<br/>', $tag_types[$token->getPartOfSpeech()->getTag()]);
-                printf('Token dependency edge: %s' . '<br/>', $token->getDependencyEdge()->serializeToJsonString());
-                print('<br/>');
-            }
-        } finally {
-            $languageServiceClient->close();
-        }
     }
 
     /**
