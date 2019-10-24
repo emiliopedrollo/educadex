7b18db25c462048be53e7d4c88fd3f3038c05c3e
Emílio B. Pedrollo <emiliopedrollo@gmail.com>
2019-10-24 13:39:29 -0300

    Add link to the real form

--- resources/views/answer.blade.php
+++ resources/views/answer.blade.php
@@ -36,7 +36,7 @@
     </div>
     <hr>
     <div class="accordion col-sm-10 offset-sm-1">
-        <a href="https://google.com">Deixe seu comentário</a>
+        <a href="https://forms.gle/aeMWbBv3j96xKvrR9">Deixe seu comentário</a>
     </div>
     @if( App::environment('local') ):
     <hr class="my-4">
